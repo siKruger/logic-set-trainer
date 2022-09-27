dc7cfdc8e373d45360d13d071f256669dec3c27f
Simon Krüger <sim.krueger@ostfalia.de>
2022-09-27 18:04:08 +0200

    switched back to string for better evaluation

--- src/expressionEvaluator.ts
+++ src/expressionEvaluator.ts
@@ -9,11 +9,11 @@ const operatorPrecedence = [
   ['<=>'],
 ];
 
-const evaluateExpression = (expression: string): string[] => {
-  const cleanedExpression = [...expression.replaceAll(/\s/g, '')];
+const evaluateExpression = (expression: string): string => {
+  let cleanedExpression = expression.replaceAll(/\s/g, '');
 
-  const openParentheses = cleanedExpression.filter((val) => val === '(');
-  const closedParentheses = cleanedExpression.filter((val) => val === ')');
+  const openParentheses = [...cleanedExpression].filter((val) => val === '(');
+  const closedParentheses = [...cleanedExpression].filter((val) => val === ')');
 
   // Check if parentheses are correct
   if (openParentheses.length !== closedParentheses.length) throw Error();
@@ -23,28 +23,33 @@ const evaluateExpression = (expression: string): string[] => {
 
   // We have some work to do.. :(
   const openParenthesesPosition: number[] = [];
-  cleanedExpression.forEach((char, index) => {
+  [...cleanedExpression].forEach((char, index) => {
     if (char === '(') openParenthesesPosition.push(index);
   });
 
-  const outputBuffer: string[][] = [];
+  const outputBuffer: string[] = [];
 
   for (let x = openParenthesesPosition.length - 1; x >= 0; x -= 1) {
     const position = openParenthesesPosition[x];
     const parenthesesPart = cleanedExpression.slice(position);
 
     for (let subIndex = 0; subIndex < parenthesesPart.length; subIndex += 1) {
+      const skippableParentheses: number[] = [];
+
       const subPart = parenthesesPart[subIndex];
-      if (subPart === ')') {
+      if (subPart === ')' && !skippableParentheses.includes(position + subIndex + 1)) {
+        skippableParentheses.push(position + subIndex + 1);
         openParenthesesPosition.pop();
-        outputBuffer.push(cleanedExpression.slice(position, position + subIndex + 1));
+        outputBuffer.push(cleanedExpression.substring(position, position + subIndex + 1));
+        cleanedExpression = cleanedExpression.replaceAll(cleanedExpression.substring(position, position + subIndex + 1), '**REPLACED EXPRESSION**');
         break;
       }
     }
   }
 
+  outputBuffer.push(cleanedExpression);
   console.log(outputBuffer);
-  return [];
+  return 'kek';
 };
 
 export default evaluateExpression;
