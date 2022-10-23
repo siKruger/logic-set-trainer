25efda2ab1f32ec8c3cd44904be14dc415f095f9
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2022-10-23 12:07:25 +0200

    ein weitere Button für euler-diagram eingefügt.

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -10,6 +10,7 @@ import Image from 'react-bootstrap/Image'
 
 const Main = () => {
   const [showVennDiagram, setShowVennDiagram] = useState(false); // for Venn-diagram
+  const [showEulerDiagram, setShowEulerDiagram] = useState(false); // for Euler-diagram
   const [expression, setExpression] = useState('');
   const [evaluatedExpression, setEvaluatedExpression] = useState<TruthtableEvaluation>();
   const getEvaluation = () => {
@@ -113,109 +114,130 @@ const Main = () => {
           </div>
         </div>
       </Container>
-      {/*erst wenn ein Parameter eingegeben wird, wird dann angezeigt. */}          
-      { evaluatedExpression && (       
-      <Container id="table_container">
-        <div id="table">
-          <div id="table_text">
-            Truth Table
-          </div>
-          <div id="table_content">
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  {evaluatedExpression?.variables.map((variable) => (
-                    <th key={variable}>
-                      {' '}
-                      {variable}
-                      {' '}
-                    </th>
-                  ))}
-                  {evaluatedExpression?.steps.map((step) => (
-                    <th key={step}>
-                      {' '}
-                      {step}
-                      {' '}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {/* todo better keys  */}
-                {
-                  evaluatedExpression !== undefined && evaluatedExpression?.variables.length === 1
-                    ? evaluatedExpression?.binaryOptions.map((binaryValue, upperIndex) => (
-                      // eslint-disable-next-line react/no-array-index-key
-                      <tr key={`${upperIndex}upperBinary`}>
+      {/*erst wenn ein Parameter eingegeben wird, wird dann angezeigt. */}
+      {evaluatedExpression && (
+        <Container id="table_container">
+          <div id="table">
+            <div id="table_text">
+              Truth Table
+            </div>
+            <div id="table_content">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    {evaluatedExpression?.variables.map((variable) => (
+                      <th key={variable}>
                         {' '}
-                        <td>
+                        {variable}
+                        {' '}
+                      </th>
+                    ))}
+                    {evaluatedExpression?.steps.map((step) => (
+                      <th key={step}>
+                        {' '}
+                        {step}
+                        {' '}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* todo better keys  */}
+                  {
+                    evaluatedExpression !== undefined && evaluatedExpression?.variables.length === 1
+                      ? evaluatedExpression?.binaryOptions.map((binaryValue, upperIndex) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <tr key={`${upperIndex}upperBinary`}>
                           {' '}
-                          {binaryValue}
-                        </td>
-                        {evaluatedExpression?.steps.map((step, lowerIndex) => (
-                          // eslint-disable-next-line react/no-array-index-key
-                          <td key={step + upperIndex + lowerIndex}>
+                          <td>
                             {' '}
-                            **PLACEHOLDER**
-                            {' '}
+                            {binaryValue}
                           </td>
-                        ))}
-                      </tr>
-                    ))
-                    : evaluatedExpression?.binaryOptions.map((binaryRow, upperIndex) => (
-                      // eslint-disable-next-line react/no-array-index-key
-                      <tr key={`${upperIndex}upperBinary1`}>
-                        {' '}
-                        {
-                          binaryRow.map((binaryValue, lowerIndex) => (
+                          {evaluatedExpression?.steps.map((step, lowerIndex) => (
                             // eslint-disable-next-line react/no-array-index-key
-                            <td key={`${upperIndex}lowerBinary1${lowerIndex}`}>
+                            <td key={step + upperIndex + lowerIndex}>
                               {' '}
-                              {binaryValue}
+                              **PLACEHOLDER**
                               {' '}
                             </td>
-                          ))
-                        }
-                        {evaluatedExpression?.steps.map((step) => (
-                          // eslint-disable-next-line react/no-array-index-key
-                          <td key={step + binaryRow + upperIndex}>
-                            {' '}
-                            **PLACEHOLDER**
-                            {' '}
-                          </td>
-                        ))}
-                      </tr>
-                    ))
-                }
-              </tbody>
-            </Table>
+                          ))}
+                        </tr>
+                      ))
+                      : evaluatedExpression?.binaryOptions.map((binaryRow, upperIndex) => (
+                        // eslint-disable-next-line react/no-array-index-key
+                        <tr key={`${upperIndex}upperBinary1`}>
+                          {' '}
+                          {
+                            binaryRow.map((binaryValue, lowerIndex) => (
+                              // eslint-disable-next-line react/no-array-index-key
+                              <td key={`${upperIndex}lowerBinary1${lowerIndex}`}>
+                                {' '}
+                                {binaryValue}
+                                {' '}
+                              </td>
+                            ))
+                          }
+                          {evaluatedExpression?.steps.map((step) => (
+                            // eslint-disable-next-line react/no-array-index-key
+                            <td key={step + binaryRow + upperIndex}>
+                              {' '}
+                              **PLACEHOLDER**
+                              {' '}
+                            </td>
+                          ))}
+                        </tr>
+                      ))
+                  }
+                </tbody>
+              </Table>
+            </div>
+            <div id="table_instead_content"></div>
           </div>
-          <div id="table_instead_content"></div>
-        </div>
 
-      </Container>
-)}
+        </Container>
+      )}
       {/* for venn-diagram  */}
       <Container>
-        {showVennDiagram && evaluatedExpression &&(
+        {showVennDiagram && evaluatedExpression && (
 
           <Button onClick={() => setShowVennDiagram(false)} variant="contained">Hide Venn-diagram</Button>
+
         )
         }
-        {!showVennDiagram && evaluatedExpression &&(
+        {!showVennDiagram && evaluatedExpression && (
 
           <Button onClick={() => setShowVennDiagram(true)} variant="contained">Show Venn-diagram</Button>
+        )}
+        {/* for euler-diagram  */}
+        {showEulerDiagram && evaluatedExpression && (
+
+          <Button onClick={() => setShowEulerDiagram(false)} variant="contained">Hide Euler-diagram</Button>
+
+        )
+        }
+        {!showEulerDiagram && evaluatedExpression && (
+
+          <Button onClick={() => setShowEulerDiagram(true)} variant="contained">Show Euler-diagram</Button>
         )}
       </Container>
 
       {showVennDiagram &&
         <Container>
-         <img
+          <img
             src="https://www.presentationload.de/blog/wp-content/uploads/Venn-Diagramm-Titelbild-16-zu-9.jpg.webp"
             className='img-thumbnail'
             alt='...'
           />
         </Container>
+      }
+      {showEulerDiagram &&
+      <Container>
+        <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/EulerDiagram.svg/800px-EulerDiagram.svg.png"
+            className='img-thumbnail'
+            alt='...'
+          />
+      </Container>
       }
 
     </>
