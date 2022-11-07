5db0e0673a5a54a03bff256e74440ec2d554e16a
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2022-11-07 16:24:03 +0100

    Neue Komponente für die Darstellung von Note und Venn-Diagramm

---
+++ src/components/Feature.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
+
+
+
+export default function Feature(props: { checkedVennDiagramm: any; evaluatedExpression: any; checkedNote: any }) {
+    return (
+        <>
+
+            {!props.checkedVennDiagramm && props.evaluatedExpression && (
+                <Container>
+                    <img
+                        src="https://www.presentationload.de/blog/wp-content/uploads/Venn-Diagramm-Titelbild-16-zu-9.jpg.webp"
+                        className='img-thumbnail'
+                        alt='...'
+                    /><br />
+                </Container>
+            )
+            }
+
+            {!props.checkedNote && props.evaluatedExpression &&
+                <Container>
+                    <div id="form">
+
+
+                        <Form >
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1"> <br />
+                                <Form.Label className="lead">Make a note</Form.Label>
+                                <Form.Control id="text" as="textarea" rows={5} placeholder="Type your note here..." />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Control id="filename" as="input" placeholder="Specify a filename..." />
+                            </Form.Group>
+
+                            <Button variant="contained" type="submit">
+                                save
+                            </Button>
+                        </Form>
+                    </div>
+                </Container>
+            }
+        </>
+    )
+}
