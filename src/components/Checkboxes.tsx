5bbe7b5fab6b1ed58df2473f487af78996281c37
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2022-11-10 17:55:42 +0100

    new design für checkbox

--- src/components/Checkboxes.tsx
+++ src/components/Checkboxes.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './Checkboxes.css'
 export default function Checkboxes(props: { checkedVennDiagramm: any; setCheckedVennDiagramm: (arg0: boolean) => void; checkedNote: any; setCheckedNote: (arg0: boolean) => void }) {
     return (
         <>
-            <label>
+            {/* <label>
                 <input type="checkbox"
                     defaultChecked={!props.checkedVennDiagramm}
                     onChange={() => props.setCheckedVennDiagramm(!props.checkedVennDiagramm)}
@@ -17,7 +17,27 @@ export default function Checkboxes(props: { checkedVennDiagramm: any; setChecked
                     onChange={() => props.setCheckedNote(!props.checkedNote)}
                 />
                 Note
-            </label>
+            </label> */}
+            <div id='checkbox-container'>
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox"
+                defaultChecked={!props.checkedVennDiagramm}
+                onChange={() => props.setCheckedVennDiagramm(!props.checkedVennDiagramm)}
+                />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                    Venn-Diagramm
+                    </label>
+            </div>
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" 
+                defaultChecked={!props.checkedNote}
+                onChange={() => props.setCheckedNote(!props.checkedNote)}
+                />
+                    <label className="form-check-label" htmlFor="flexCheckChecked">
+                    Note
+                    </label>
+            </div>
+            </div>
         </>
     )
 }
