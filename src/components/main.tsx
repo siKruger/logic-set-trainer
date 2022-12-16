0870c1c052a59d579eaa23ff7e2158db05d465e3
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2022-12-16 10:54:43 +0100

    alle unnötige Kommentare weg

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -41,25 +41,6 @@ const Main = () => {
     }
   };
 
-  // const exportAsImage = async (el: any, imageFileName: any) => {
-  //   const canvas = await html2canvas(el);
-  //   const image = canvas.toDataURL("image/png", 1.0);
-  //   downloadImage(image, imageFileName);
-  // };
-
-  // const downloadImage = (blob: any, fileName: string) => {
-  //   const fakeLink = window.document.createElement("a");
-  //   // fakeLink.style = "display:none;";
-  //   fakeLink.download = fileName;
-
-  //   fakeLink.href = blob;
-
-  //   document.body.appendChild(fakeLink);
-  //   fakeLink.click();
-  //   document.body.removeChild(fakeLink);
-
-  //   fakeLink.remove();
-  // };
 
   return (
     <>
